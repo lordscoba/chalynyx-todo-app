93e9b72d10c83194ddef692ea7995dd0e9df348e
lordscoba <lordscoba2tm@gmail.com>
2023-05-15 12:18:32 -0700

    chalynyx todo app

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  final String title = "Chalynyx To do App";
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Center(
+          child: Text(title),
+        ),
+      ),
+    );
+  }
+}
